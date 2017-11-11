0390d9d4d3c7ab34adb2f40bb5ae032454064750
Imma coding <benworthh@gmail.com>
2017-11-11 18:04:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-if (string === stirng.toLowerCase())
+if (string === string.toLowerCase())
   return "I can't hear you!"
 }
